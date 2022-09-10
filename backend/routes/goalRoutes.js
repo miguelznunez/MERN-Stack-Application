8b0c7d4c2816ba22ab7ac3c47cbd87677124411a
Miguel <iamcodefoxx@gmail.com>
2022-09-10 14:00:03 -0700

    Mern Stack setup and jwt auth

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const router = express.Router()
+const { getGoals,
+        setGoal,
+        updateGoal,
+        deleteGoal } = require("../controllers/goalController")
+
+const { protect } = require("../middleware/authMiddleware")
+
+router.get("/", protect, getGoals)
+
+router.post("/", protect, setGoal)
+
+router.put("/:id", protect, updateGoal)
+
+router.delete("/:id", protect, deleteGoal)
+
+module.exports = router
